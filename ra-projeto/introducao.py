8bedd77f9e1d3f938e0d96f0c555134bed96c43a
Raí <lamperdeavila@gmail.com>
2024-02-23 11:26:25 -0300

    Add files via upload

---
+++ ra-projeto/introducao.py
@@ -0,0 +1,48 @@
+print("matos")
+
+'''bloco de notas(ignora as linhas)
+'''
+
+#comentarios
+print("matos," , "muri", end="--")
+print("matos é legal, muri nao")
+
+'''tipos primitivos de dados:
+numericos inteiros(int)
+numericos reais(float)-->floating point
+texto(string)
+booleanos(bool)-->true false
+'''
+
+print(1, 2, 3, 4)#int
+print(1.1, 2.2, 3.3, 4.4)#float
+print(True, False)#bool
+#string
+print('ola, matos')
+print('''ola matos, vc é bacana''')
+
+#uso de escape char(\)
+print("ola, \"matos\"")
+print("matos\nchedar")
+print("C:\\Projeto")
+
+print(2)
+print(2.0)
+print("2")
+print("2.0")
+
+print("------------")
+print(print("chedar"))
+
+#entrada e saida de dados (io)
+input("informe seu nome:")
+
+nome = input("informe o seu nome:")
+print("ola,", nome)
+
+idade = 20
+print("chedar tem", idade, "anos")
+
+matricula_aluno = "2024"
+#maiuscula quer dizer que é constante(somente para as pessoas)
+MAX_VALORES = 30
